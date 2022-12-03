acd7f9bcd4003a0981f78280111db32b128203d2
Milo Todorovich <milo.todorovich@gmail.com>
2022-12-03 11:00:38 -0600

    Day 03 part 2 & cleanup

--- 03.rb
+++ 03.rb
@@ -5,30 +5,32 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
-HLVHsVWLwbWswbpWFWrrmThfTPNnhNSDDNhDfznTnhnS
 INPUT
 
-input = File.read("data/input03.txt")
+# input = File.read("data/input03.txt")
+
+def priority(c)
+    if c == c.upcase
+        c.ord - 38
+    else
+        c.ord - 96
+    end
+end
 
 rucksacks = input.split("\n")
-puts rucksacks
 
 sum = rucksacks.map do |sack|
-    puts sack.chars.first(sack.size/2).join
-    puts sack.chars.last(sack.size/2).join  
-    puts sack.chars.first(sack.size/2).intersection(sack.chars.last(sack.size/2)).first
     sack.chars.first(sack.size/2).intersection(sack.chars.last(sack.size/2)).first
 end.map do |c|
-    puts c.chr
-    puts c.ord
-    value = c.ord
-    if c == c.upcase
-        value = value - 38
-    else
-        value = value - 96
-    end
-    puts value
-    value
+    priority(c)
 end.sum
 
-puts "Sum: #{sum}"
+puts "Part 1 - Sum: #{sum}"
+
+
+sum = rucksacks.each_slice(3).map do |a|
+    a[0].chars.intersection(a[1].chars).intersection(a[2].chars).first
+end.map do |x|
+    priority(x)
+end.sum
+puts "Part 2 - Sum: #{sum}"
