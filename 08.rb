2c277d03e9d70d7615e2ba91d51a5f82993fd7e8
Milo Todorovich <milo.todorovich@gmail.com>
2022-12-08 20:48:06 -0600

    Day 8 part II

--- 08.rb
+++ 08.rb
@@ -17,45 +17,64 @@ rows[0].length.times do |i|
     end
     cols << col
 end
-pp rows
-pp cols
 
 visibles = []
 
 rows.each_with_index do |row, x| 
     visible_row = []
     row.each_with_index do |tree, y|
-        puts "Tree: #{tree}"
-        puts "Row: #{row}"
-        puts "Col: #{cols[y]}"
         left = row.slice(0, y)
         right = row.slice(y+1, row.length)
         up = cols[y].slice(0, x)
         down = cols[y].slice(x+1, cols[y].length)
-        puts "Max: L:#{left} R:#{right} U:#{up} D:#{down}"
-        max_left = row.slice(0, y).max || -1
-        max_right = row.slice(y+1, row.length).max || -1
-        max_up = cols[y].slice(0, x).max || -1
-        max_down = cols[y].slice(x+1, cols[y].length).max || -1
-        # puts "--"
-        puts "Max: L:#{max_left} R:#{max_right} U:#{max_up} D:#{max_down}"
+        
+        max_left = left.max || -1
+        max_right = right.max || -1
+        max_up = up.max || -1
+        max_down = down.max || -1
+
         visible_left = tree > max_left
         visible_right = tree > max_right
         visible_up = tree > max_up
         visible_down = tree > max_down
-        puts "Visibles: #{visible_left} #{visible_right} #{visible_up} #{visible_down}"
+
         visible = (
             visible_left || 
             visible_right || 
             visible_up || 
             visible_down )
-        puts visible
         visible_row << visible
-        # pp visible_row
-        puts "--"
     end
     visibles << visible_row
 end
-pp rows
-pp visibles
 puts "Part I: #{visibles.flatten.select { |t| t }.count}"
+
+
+def view_score(height, line)
+    score = 0
+    while (score < line.length)
+        score = score + 1
+        break if line.first(score).max >= height
+    end
+    score
+end
+
+scores = []
+rows.each_with_index do |row, x| 
+    score_row = []
+    row.each_with_index do |tree, y|
+        left = row.slice(0, y)
+        right = row.slice(y+1, row.length)
+        up = cols[y].slice(0, x)
+        down = cols[y].slice(x+1, cols[y].length)
+
+        score_left = view_score(row[y], left.reverse)
+        score_right = view_score(row[y], right)
+        score_up = view_score(row[y], up.reverse)
+        score_down = view_score(row[y], down)
+        
+        score_row << (score_left * score_right * score_up * score_down)
+    end
+    scores << score_row
+end
+puts "Part II: #{scores.flatten.max}"
