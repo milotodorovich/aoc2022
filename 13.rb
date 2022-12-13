c120229aa7634896c12099e11cce32be9441380f
Milo Todorovich <milo.todorovich@gmail.com>
2022-12-13 15:20:33 -0600

    Day 13 part 1, with passing tests, and failing on the real data

--- 13.rb
+++ 13.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 input = <<INPUT
 [1,1,3,1,1]
 [1,1,5,1,1]
@@ -24,57 +26,63 @@ input = <<INPUT
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 INPUT
 
-# input = File.read("data/input13.txt")
+input = File.read("data/input13.txt")
 
 pair_strings = input.split("\n\n")
 pairs = pair_strings.map { |s| s.split("\n") }
 
 def compare(left, right)
     puts "Comparing #{left} and #{right}"
-    left.each_with_index do |l, i|
-        r = right[i]
-        return false if r.nil?
 
-        if l.is_a?(Array) || r.is_a?(Array) 
-            result = compare(Array(l), Array(r))
-            return false unless result
-        else
-            return true if l < r
-            return false if l > r
-        end
+    return false if right.nil?
+    if left.is_a?(Numeric) && right.is_a?(Numeric)
+        return left <= right
+    end
+    if left.is_a?(Array) && right.is_a?(Numeric)
+        return compare(left, Array(right))
+    end
+    if left.is_a?(Numeric) && right.is_a?(Array)
+        return compare(Array(left), right)
+    end
+    left.each_with_index do |l, i|
+        next if l == right[i]
+        result = compare(l, right[i])
+        return result
     end
 
     return true
 end
 
-puts compare(eval(pairs[0].first), eval(pairs[0].last))
-puts true
-puts compare(eval(pairs[1].first), eval(pairs[1].last))
-puts true
-puts compare(eval(pairs[2].first), eval(pairs[2].last))
-puts compare(eval(pairs[3].first), eval(pairs[3].last))
-puts true
-puts compare(eval(pairs[4].first), eval(pairs[4].last))
-puts compare(eval(pairs[5].first), eval(pairs[5].last))
-puts true
-puts compare(eval(pairs[6].first), eval(pairs[6].last))
-puts compare(eval(pairs[7].first), eval(pairs[7].last))
+# puts compare(eval(pairs[0].first), eval(pairs[0].last))
+# puts true
+# puts compare(eval(pairs[1].first), eval(pairs[1].last))
+# puts true
+# puts compare(eval(pairs[2].first), eval(pairs[2].last))
+# puts compare(eval(pairs[3].first), eval(pairs[3].last))
+# puts true
+# puts compare(eval(pairs[4].first), eval(pairs[4].last))
+# puts compare(eval(pairs[5].first), eval(pairs[5].last))
+# puts true
+# puts compare(eval(pairs[6].first), eval(pairs[6].last))
+# puts compare(eval(pairs[7].first), eval(pairs[7].last))
 # exit
 
 results = pairs.map do |p|
     left, right = *p
     puts "L: #{left}"
     puts "R: #{right}"
-    r = compare(eval(left), eval(right))
+    r = compare(JSON.parse(left), JSON.parse(right))
     puts r 
     puts "--"
     r
 end
 
-# pp results
+pp results
 
 sum = results.each_with_index.reduce(0) do |memo, (element, index)|
-    memo = memo + index + 1 if element
+    if element
+        memo = memo + index + 1 
+    end
     memo
 end
 
---
+++ 13_test.rb
@@ -0,0 +1,63 @@
+require "test/unit"
+require_relative './13'
+
+class ThirteenTest < Test::Unit::TestCase
+
+    def test_compare_two_digits
+        assert_true compare(1, 1)
+        assert_true compare(0, 1)
+        assert_false compare(1, 0)
+    end
+
+    def test_compare_two_arrays_same_size
+        assert_true compare([1,2], [1,2])
+        assert_true compare([1,2], [1,3])
+        assert_true compare([1,2], [2,3])
+        assert_false compare([3,2], [2,3])
+    end
+
+    def test_compare_two_arrays_first_smaller
+        assert_true compare([4], [4,4])
+        assert_true compare([3], [4,4])
+        assert_false compare([5], [4,4])
+    end
+
+    def test_compare_two_arrays_second_smaller
+        assert_false compare([4, 4], [4])
+        assert_true compare([3, 4], [4])
+    end
+
+    def test_array_and_number_in_order
+        assert_true compare([2,3,4], 4)
+    end
+    def test_array_and_number_NOT_in_order
+        assert_false compare([2,3,4], 1)
+    end
+
+    def test_1
+        assert_true compare([1,1,3,1,1], [1,1,5,1,1])
+    end
+    def test_2
+        assert_true compare([[1],[2,3,4]], [[1],4])
+    end
+    def test_3
+        assert_false compare([9], [[8,7,6]])
+    end
+    def test_4
+        assert_true compare([[4,4],4,4], [[4,4],4,4,4])
+    end
+    def test_5
+        assert_false compare([7,7,7,7], [7,7,7])
+    end
+    def test_6
+        assert_true compare([], [3])
+    end
+    def test_7
+        assert_false compare([[[]]], [[]])
+    end
+    def test_8
+        assert_false compare(
+            [1,[2,[3,[4,[5,6,7]]]],8,9],
+            [1,[2,[3,[4,[5,6,0]]]],8,9])
+    end
+end
